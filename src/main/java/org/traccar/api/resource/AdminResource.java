aff96ec993b62a35dfc1f6e425a2f40c2dd94d68
Suryakanta Parida <surya@ridesiq.com>
2026-05-07 23:11:53 +0530

    build issue

--- src/main/java/org/traccar/api/resource/AdminResource.java
+++ src/main/java/org/traccar/api/resource/AdminResource.java
@@ -32,7 +32,6 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
